f81e5ae98fd894dbad88c044f5faa3fc40cfd68f
agus-darmawan <wayanagus.dr@gmail.com>
2025-09-09 12:01:42 +0700

    feat: fix map size when zoom out

--- CWSDirectoryMaps/Views/IntegratedMapView.swift
+++ CWSDirectoryMaps/Views/IntegratedMapView.swift
@@ -733,7 +733,7 @@ struct ZoomableScrollView<Content: View>: View {
                                 offset = clampedOffset(offset, geo: geo)
                             }
                             .onEnded { _ in
-                                lastScale = max(min(scale, 6.0), 0.5)
+                                lastScale = max(min(scale, 6.0), 2)
                                 scale = lastScale
                                 offset = clampedOffset(offset, geo: geo)
                                 lastOffset = offset
